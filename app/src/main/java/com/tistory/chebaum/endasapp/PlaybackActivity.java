7342322a3962daa84f715c9db1cd8c60824965c9
chebaum <chebaum@naver.com>
2018-01-09 17:05:13 +0900

    added sample video for VideoView / new activity added

---
+++ app/src/main/java/com/tistory/chebaum/endasapp/PlaybackActivity.java
@@ -0,0 +1,11 @@
+package com.tistory.chebaum.endasapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by cheba on 2018-01-09.
+ */
+
+public class PlaybackActivity extends AppCompatActivity {
+    
+}
